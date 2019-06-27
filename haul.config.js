23ecdd3a6efe48d79ccb3dd0bd2d056383086d15
Jorge Bernal <jbernal@gmail.com>
2019-06-27 13:05:21 +0200

    Make haul work
    Somehow the default configuration was causing an error in the app

--- haul.config.js
+++ haul.config.js
@@ -1,7 +1,19 @@
-import { createWebpackConfig } from "haul";
+import { createWebpackConfig } from 'haul';
 
 export default {
-  webpack: createWebpackConfig(({ platform }) => ({
-    entry: `./index.js`
-  }))
+  webpack: (env) => {
+    const config = createWebpackConfig({
+      entry: `./index.js`
+    })(env);
+
+    config.module.rules = [
+      {
+        test: /\.js?$/,
+        loaders: ['babel-loader']
+      },
+      ...config.module.rules
+    ];
+
+    return config;
+  }
 };
