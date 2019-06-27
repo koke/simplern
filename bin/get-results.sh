3ea65603286794d86e9555a82cc8dc76eaebbf62
Jorge Bernal <jbernal@gmail.com>
2019-06-27 16:45:31 +0200

    Include debugger code in the results

--- bin/get-results.sh
+++ bin/get-results.sh
@@ -30,11 +30,17 @@ curl -so "${resultsDir}/metro-index.js" "http://localhost:${metroPort}/index.bun
 # Get source map from Metro
 curl -s "http://localhost:${metroPort}/index.map?platform=ios&dev=true&minify=false" | jq . > "${resultsDir}/metro-map.json"
 
+# Get debugger from Metro
+curl -o "${resultsDir}/metro-debugger.js" "http://localhost:${metroPort}/debugger-ui/debuggerWorker.js"
+
 # Get source from Haul
 curl -so "${resultsDir}/haul-index.js" "http://localhost:${haulPort}/index.bundle?platform=ios&dev=true&minify=false"
 
 # Get source map from Haul
 curl -s "http://localhost:${haulPort}/index.ios.bundle.map" | jq . > "${resultsDir}/haul-map.json"
+
+# Get debugger from Haul
+curl -o "${resultsDir}/haul-debugger.js" "http://localhost:${haulPort}/debuggerWorker.js"
 
 # Stop metro and haul before we go
 kill $metroPid
