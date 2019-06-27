bf628c1cbd1277058b611726515b55e7c648895a
Jorge Bernal <jbernal@gmail.com>
2019-06-27 13:44:21 +0200

    Don't use eval and embedded source maps

--- haul.config.js
+++ haul.config.js
@@ -1,19 +1,42 @@
 import { createWebpackConfig } from 'haul';
+import webpack from 'webpack';
+import merge from 'webpack-merge';
 
 export default {
   webpack: (env) => {
-    const config = createWebpackConfig({
-      entry: `./index.js`
-    })(env);
+		const defaultConfig = createWebpackConfig( {
+			entry: `./index.js`,
+		} )( env );
 
-    config.module.rules = [
-      {
-        test: /\.js?$/,
-        loaders: ['babel-loader']
-      },
-      ...config.module.rules
-    ];
+    // By default, haul registers some plugins to generate source maps.
+		// Webpack will ignore our devtool configuration if those plugins are present.
+		const pluginBlacklist = new Set( [
+			webpack.SourceMapDevToolPlugin,
+			webpack.EvalSourceMapDevToolPlugin,
+		] );
+		defaultConfig.plugins = defaultConfig.plugins.filter( ( plugin ) => ! pluginBlacklist.has( plugin.constructor ) );
 
-    return config;
+		return merge(
+			defaultConfig,
+			{
+				module: {
+					rules: [
+						{
+							test: /\.js?$/,
+							loaders: [ 'babel-loader' ],
+						},
+					],
+				},
+				plugins: [
+					new webpack.SourceMapDevToolPlugin( {
+						test: /\.(js|css|(js)?bundle)($|\?)/i,
+						filename: '[file].map',
+						publicPath: defaultConfig.output.publicPath,
+						moduleFilenameTemplate: '[absolute-resource-path]',
+					} ),
+				],
+				devtool: false,
+			}
+		);
   }
 };
