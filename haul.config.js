ecb2117e91f99c4a58a7691db573e36638a06476
Jorge Bernal <jbernal@gmail.com>
2019-07-02 17:01:44 +0200

    Disable EvalSourceMapDevToolPlugin

--- haul.config.js
+++ haul.config.js
@@ -1,10 +1,19 @@
 import { createWebpackConfig } from 'haul';
+import webpack from 'webpack';
 
 export default {
   webpack: (env) => {
     const config = createWebpackConfig({
       entry: `./index.js`
     })(env);
+
+    // By default, haul registers some plugins to generate source maps.
+		// Webpack will ignore our devtool configuration if those plugins are present.
+		const pluginBlacklist = new Set( [
+			// webpack.SourceMapDevToolPlugin,
+			webpack.EvalSourceMapDevToolPlugin,
+		] );
+		config.plugins = config.plugins.filter( ( plugin ) => ! pluginBlacklist.has( plugin.constructor ) );
 
     config.module.rules = [
       {
