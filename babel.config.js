c902b6d2928bd727c7c9244badf8ff946f546a5a
Jorge Bernal <jorge@automattic.com>
2019-07-05 11:55:34 +0200

    Switch to haul/next

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:@haul-bundler/babel-preset-react-native'],
   };
 };
--- haul.config.js
+++ haul.config.js
@@ -1,19 +1,9 @@
-import { createWebpackConfig } from 'haul';
+import { withPolyfills, makeConfig } from "@haul-bundler/preset-0.59";
 
-export default {
-  webpack: (env) => {
-    const config = createWebpackConfig({
-      entry: `./index.js`
-    })(env);
-
-    config.module.rules = [
-      {
-        test: /\.js?$/,
-        loaders: ['babel-loader']
-      },
-      ...config.module.rules
-    ];
-
-    return config;
-  }
-};
+export default makeConfig({
+  bundles: {
+    index: {
+      entry: withPolyfills('./index.js'),
+    },
+  },
+});
